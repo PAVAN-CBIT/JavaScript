062642fcca47b40aba8c6bf05febcfcf71419e29
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2022-02-16 12:41:28 +0530

    Create  Importing_module_into_another_module.js

---
+++ Importing_module_into_another_module.js
@@ -0,0 +1,69 @@
+   // library.js
+===============================================================================================================================================
+ 
+ import { PrintCategory } from "./Category.js";
+ 
+ var title="Product Details";
+
+
+    
+
+   export  function PrintTitle(){
+        return title;
+    }
+  export default   function PrintDetails(id,name,price){
+
+         return `ID=${id}<br>Name=${name}<br>Price=${price}<br>Category=${PrintCategory("Electronics")}`;
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+    //Category.js
+==================================================================================================================================================================    
+    
+    
+    
+     export  function PrintCategory(str){
+
+    return str;
+
+ }
+
+
+
+
+
+
+ //index.html
+===========================================================================================================================================================
+  
+  
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="module">
+                        import PrintDetails,{PrintTitle} from '../Library/library.js';
+
+                        document.querySelector("h2").innerHTML=PrintTitle();
+                        document.querySelector("p").innerHTML=PrintDetails(1,'Samsung',45000);
+
+    </script>
+</head>
+<body>
+ <h2></h2>
+ <p></p>
+</body>
+</html>
