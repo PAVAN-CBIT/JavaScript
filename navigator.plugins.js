92bded4e6f22c32aa4bd1e2ac0eaab469f8be3a2
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2022-02-25 11:16:58 +0530

    Create navigator.plugins.js

---
+++ navigator.plugins.js
@@ -0,0 +1,9 @@
+<script>
+    if(navigator.plugins['PDF Viewer']==undefined){
+        alert('Please Install PDF Viewr');
+        location.href='https://www.adobe.com/in/creativecloud.html?sdid=SL4KMHQ2&mv=search&ef_id=CjwKCAiA9tyQBhAIEiwA6tdCrH1M9OAv4FWA6magraYkHtJ-6RWyUnrjWoCh41j_Tdz3O3nDSChdbBoCfIYQAvD_BwE:G:s&s_kwcid=AL!3085!3!446722869342!e!!g!!adobe!221174348!17525765828&gclid=CjwKCAiA9tyQBhAIEiwA6tdCrH1M9OAv4FWA6magraYkHtJ-6RWyUnrjWoCh41j_Tdz3O3nDSChdbBoCfIYQAvD_BwE';
+
+    }else{
+        document.write('You can watch PDF Viewer');
+    }
+</script>
