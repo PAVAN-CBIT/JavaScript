56dabb0a526b84eefb065729e9abbc34e05f779c
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2022-02-26 10:25:10 +0530

    Create HistoryObject.js

---
+++ HistoryObject.js
@@ -0,0 +1,8 @@
+<script>
+    if(history.length<3){
+        document.write(`You can watch max 3 pages for free`);
+    }else{
+        alert('Please Register for more...');
+        location.href='..public/forms.html';
+    }
+</script>
